606f30c5fffd2b383861c83a778b9361bdc6cbdc
Luis Gustavo Soares <luissoaresg09@gmail.com>
2023-10-06 16:51:45 -0300

    terabyte

---
+++ js/table.js
@@ -0,0 +1,9 @@
+function tableInsert(produto){
+    produto.forEach(produto => {
+        insertProd(produto)
+    });
+}
+
+function insertProd(p){
+    
+}
---
+++ js/variaveis.js
@@ -0,0 +1,13 @@
+let id          = document.getElementById("id");
+let descri      = document.getElementById("descricao");
+let preco       = document.getElementById("preco");
+let tipoMais    = document.getElementById("vendidos");
+let tipoLan     = document.getElementById("lancamentos");
+
+let btnCad      = document.getElementById("cadastrar");
+let btnEdit     = document.getElementById("alterar");
+let btnRemove   = document.getElementById("remover");
+let btnCancel   = document.getElementById("cancelar");
+
+let url = ("http://localhost:8080/");
+
